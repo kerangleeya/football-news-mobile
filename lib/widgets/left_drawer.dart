9a56bbc4f4a96ea31409d59e61e55a7ffec6014e
kerangleeya <shelia.vellicita13@gmail.com>
2025-11-12 14:56:24 +0700

    Finishing Tutorial 8

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:football_news/screens/menu.dart';
 import 'package:football_news/screens/newslist_form.dart';
+import 'package:football_news/screens/news_entry_list.dart';
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
@@ -62,10 +63,16 @@ class LeftDrawer extends StatelessWidget {
                   ));
             },
           ),
-          // TODO: Create another ListTile for the news list page
           ListTile(
-            leading: const Icon(Icons.list),
+            leading: const Icon(Icons.add_reaction_rounded),
             title: const Text('News List'),
+            onTap: () {
+                // Route to news list page
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const NewsEntryListPage()),
+                );
+            },
           ),
         ],
       ),
